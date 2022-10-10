5b0fa09da074046356ea4f1768329129e4747589
matiunivar <mattff3@gmail.com>
2022-10-10 17:09:19 -0300

    Improving code UI

--- src/helpers/filters.js
+++
@@ -1,9 +0,0 @@
-/** Vue Filters Start */
-Vue.filter('truncate', function (text, length, suffix) {
-    if (text.length > length) {
-        return text.substring(0, length) + suffix;
-    } else {
-        return text;
-    }
-});
-/** Vue Filters End */
